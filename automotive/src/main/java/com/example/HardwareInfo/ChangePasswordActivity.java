108152916b223e043344e64c9a73ff8d6c3de3c4
Koushik Jeyaram <koushikjeyaram19@gmail.com>
2024-07-22 13:55:25 +0530

    signin-commit-4

--- automotive/src/main/java/com/example/HardwareInfo/ChangePasswordActivity.java
+++ automotive/src/main/java/com/example/HardwareInfo/ChangePasswordActivity.java
@@ -2,14 +2,20 @@ package com.example.HardwareInfo;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class ChangePasswordActivity extends AppCompatActivity {
 
     private EditText editPassword;
+    private TextInputLayout passwordLayout;
+    private boolean isPasswordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +23,24 @@ public class ChangePasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_change_password);
 
         editPassword = findViewById(R.id.etPassword);
+        passwordLayout = findViewById(R.id.etPasswordLayout);
         Button btnSave = findViewById(R.id.btnSave);
 
+        passwordLayout.setEndIconDrawable(R.drawable.ic_visibility_off);
+
+        passwordLayout.setEndIconOnClickListener(v -> {
+            if (isPasswordVisible) {
+                editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                passwordLayout.setEndIconDrawable(R.drawable.ic_visibility_off);
+            } else {
+                editPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                passwordLayout.setEndIconDrawable(R.drawable.ic_visibility_on);
+            }
+            isPasswordVisible = !isPasswordVisible;
+            editPassword.setSelection(editPassword.length());
+        });
+
+        // Save button click listener
         btnSave.setOnClickListener(v -> {
             String newPassword = editPassword.getText().toString().trim();
 
